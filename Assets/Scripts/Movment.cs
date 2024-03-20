d2a43f291de311c2b2024f44b211425d1ef3c276
vvbocko <wbocko05@gmail.com>
2024-03-20 20:48:50 +0100

    Simple animations & tile asset use
    Swapped from circle to RedHood (collider replaced by radius only)
    
    Created Player Animation controller:
    Idle, run and simple jump animation (sliced into 3 sections - jump up, in air, landig)
    
    Background Forest assets:
    Tile pallette adjustments
    1 platform rule tile crated

--- Assets/Scripts/Movment.cs
+++ Assets/Scripts/Movment.cs
@@ -4,18 +4,37 @@ using UnityEngine;
 
 public class Movment : MonoBehaviour
 {
-    [SerializeField] Rigidbody2D rigidBody;
-    [SerializeField] CircleCollider2D circleCollider;
-    [SerializeField] float movementSpeed = 800f;
-    [SerializeField] float jumpForce = 80f;
-    [SerializeField] float castDistance = 0.1f;
-    [SerializeField] bool isGrounded = true;
-    [SerializeField] int maxJumps = 1;
-    [SerializeField] int jumpCount;
+    [SerializeField] private Rigidbody2D rigidBody;
+    [SerializeField] private Animator animator;
+    [SerializeField] private float radius = 0.5f;
+    [SerializeField] private float movementSpeed = 800f;
+    [SerializeField] private float jumpForce = 80f;
+    [SerializeField] private float castDistance = 0.1f;
+    [SerializeField] private bool isGrounded = true;
+    [SerializeField] private int maxJumps = 1;
+    [SerializeField] private int jumpCount;
+    private bool isMoving = false;
+
+    private bool isLookingRight = true;
     private void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         rigidBody.velocity = new Vector2(horizontal * movementSpeed * Time.deltaTime, rigidBody.velocity.y);
+        
+        isMoving = horizontal != 0.0f;
+        if(horizontal > 0 && !isLookingRight || horizontal < 0 && isLookingRight)
+        {
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1.0f;
+
+            transform.localScale = localScale;
+            isLookingRight = !isLookingRight;
+
+
+        }
+
+        animator.SetBool("isMoving", isMoving);
+        animator.SetBool("isGrounded", isGrounded);
 
         bool jump = Input.GetButtonDown("Jump");
         if (jump)
@@ -31,7 +50,7 @@ public class Movment : MonoBehaviour
 
     void FixedUpdate()
     {
-        Collider2D[] overlapResults = Physics2D.OverlapCircleAll(transform.position + Vector3.down * castDistance, circleCollider.radius);
+        Collider2D[] overlapResults = Physics2D.OverlapCircleAll(transform.position + Vector3.down * castDistance, radius);
         isGrounded = false;
 
         for (int i = 0; i < overlapResults.Length; i++)
